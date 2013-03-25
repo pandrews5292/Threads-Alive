d0f98d1404c60618d76a45e775fe0c0a61855610
ggallo <ggallo@fandm.edu>
2013-03-24 21:11:03 -0400

    added declarations to ta.h

--- threadsalive.c
+++ threadsalive.c
@@ -2,18 +2,22 @@
 
 #include "threadsalive.h"
 
+static void ta_libinit(void) {
+  queue* t_queue = create_queue();
+}
+
 static void ta_create(void (*func)(void*), void* arg) {
   ucontext_t* new_thread = malloc(sizeof(ucontext_t));
   void* new_stack = malloc(524288);
   new_thread->uc_stack.ss_sp = new_stack;
   new_thread->uc_stack.ss_size = sizeof(new_stack);
   new_thread->uc_link = new_thread;//POINTER_TO_QUEUE_HEAD
-  makecontext(new_thread, func, 1, &arg);
+  makecontext(new_thread, &func, 1, &arg);
   swapcontext(&filler, new_thread);
 }
 
 static void ta_yield(void){
-    
+  
 }
 
   
--- threadsalive.h
+++ threadsalive.h
@@ -1,5 +1,12 @@
-#include <stdlib.h>
-#include <ucontext.h>
 
-void ta_yield(void);
+#include <stdlib.h>
+#include <stdio.h>
+#include <ucontext.h>
+#include "queue.h"
+
+static void ta_libinit();
+static void ta_create(void *(void*), void*);
+static void ta_yield(void);
+static int ta_waitall();
+
 
