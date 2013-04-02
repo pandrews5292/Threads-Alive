bcffc05315e5a94b07c8ec2ae116255753decd27
ggallo <ggallo@fandm.edu>
2013-04-01 22:25:14 -0400

    early stage2

--- ta_mutex.c
+++ ta_mutex.c
@@ -1,1 +1,35 @@
 // ta_mutex.c
+
+#include "ta_mutex.h"
+
+static int guard = 0;
+
+void ta_sem_init(tasem_t* sema, int value) {
+  sema = (tasem_t*)malloc(sizeof(tasem_t));
+  sema->value = value;
+  sema->w_queue = create_queue();
+}
+
+void ta_sem_destroy(tasem_t* sema) {
+  destroy_queue(sema->w_queue);
+  free(sema);
+}
+
+void ta_sem_signal(tasem_t* sema) {
+  while(cas(&guard, 0, 1));
+  
+	
+}
+
+int cas(int* ptr, int old, int new) {
+  unsigned char ret;
+
+  __asm__ __volatile__ (
+	"  lock\n"
+	"  cmpxchgl %2,%1\n"
+	"  sete %0\n"
+	: "=q" (ret), "=m" (*ptr)
+        : "r" (new), "m" (*ptr), "a" (void)
+        : "memory");
+  return ret;
+}
--- ta_mutex.h
+++ ta_mutex.h
@@ -1,1 +1,17 @@
 // ta_mutex.h
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ucontext.h>
+#inculde "queue.h"
+
+typedef struct {
+  int counter;
+  queue* waitlist;
+} tasem_t;
+
+typedef struct {
+  tasem_t* sem;
+} talock_t;
+
+void ta_sem_init(
