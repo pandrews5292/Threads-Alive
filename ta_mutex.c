0ae87bdf7a894e519d3a5c7e1bb5675dbf949fd8
Patrick Andrews <pandrews5292@gmail.com>
2013-04-01 22:18:58 -0400

    lock implementation

--- ta_mutex.c
+++ ta_mutex.c
@@ -1,1 +1,19 @@
 // ta_mutex.c
+
+void ta_lock_init(talock_t* lock){
+    lock = (talock_t*)malloc(sizeof(talock_t));
+    ta_sem_init(lock->sem, 1);
+}
+
+void ta_lock_destroy(talock_t* lock){
+    ta_sem_destroy(lock->sem);
+    free(lock);
+}
+
+void ta_lock(talock_t* lock){
+    ta_sem_wait(lock->sem);
+}
+
+void ta_unlock(talock_t* lock){
+    ta_sem_signal(lock->sem);
+}
