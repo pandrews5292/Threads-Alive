667e227c6c087d3a7eeacc4150f6d80328217b6c
Patrick Andrews <pandrews5292@gmail.com>
2013-04-07 22:34:32 -0400

    comments added

--- threadsalive.c
+++ threadsalive.c
@@ -10,6 +10,8 @@ static int sema_flag = 0;
 
 
 tcb* tcb_init() {
+    //initialize a new tcb which calls getcontext() to 
+    //hold the current context in a member called ctx.
     tcb* new_tcb = (tcb*)malloc(sizeof(tcb));
     new_tcb->ctx = (ucontext_t*)malloc(sizeof(ucontext_t));
     getcontext(new_tcb->ctx);
@@ -19,13 +21,16 @@ tcb* tcb_init() {
 }
 
 void ta_libinit(void) {
-    t_queue = create_queue();
+    //initialize the ta library
+    t_queue = create_queue(); //scheduling queue
 
-    s_queue = sema_create_queue();
+    s_queue = sema_create_queue();  //queue to hold all initizalized semaphores
     main_ctx = (ucontext_t*)malloc(sizeof(ucontext_t));
 }
 
 void ta_create(void (*func)(void*), void* arg) {
+    //create a new tcb/context which will run the desired func
+    //when swapped to
     
     tcb* new_thread = tcb_init();
     
@@ -33,24 +38,22 @@ void ta_create(void (*func)(void*), void* arg) {
     new_thread->ctx->uc_stack.ss_size = STACK_SIZE;
     new_thread->ctx->uc_link = main_ctx;
     
-    if (arg)
-	makecontext(new_thread->ctx, func, 1, arg);
-    else
-	makecontext(new_thread->ctx, func, 0);
+    makecontext(new_thread->ctx, func, 1, arg);
     
     push(t_queue, new_thread);
     
 }
 
 void ta_yield(void) {
+    //yield to a new context
     
     if (len(t_queue)) {
-	tcb* next_thread = pop(t_queue);
+	tcb* next_thread = pop(t_queue);  //grab a new tcb
 	ucontext_t* next_ctx = next_thread->ctx;
 	next_ctx->uc_link = main_ctx;
 	free(next_thread);
 	
-	tcb* cur_thread = tcb_init();   
+	tcb* cur_thread = tcb_init();  //create a new tcb to hold current context
 	
 	push(t_queue, cur_thread);
 	swapcontext(cur_thread->ctx, next_ctx); 
@@ -59,6 +62,7 @@ void ta_yield(void) {
 }
 
 int ta_waitall(void) {
+    // run all contexts created by ta_create
     
     if (!len(t_queue))
 	return -1;
@@ -72,7 +76,7 @@ int ta_waitall(void) {
 	next_ctx->uc_link = main_ctx;
 	swapcontext(main_ctx, next_ctx);
     }
-    while(sema_len(s_queue)){
+    while(sema_len(s_queue)){     //return 1 if some thread was left in sema_queue
         tasem_t* sem = sema_pop(s_queue);
         if (len(sem->w_queue)){
             sema_flag = 1;
@@ -80,24 +84,28 @@ int ta_waitall(void) {
         }
     }
     sema_destroy_queue(s_queue);
-
+    destroy_queue(t_queue);
+    
     return sema_flag;
 }
 
 void ta_sem_init(tasem_t* sema, int value) {
-  sema->value = value;
-  sema->w_queue = create_queue();
-  sema_push(s_queue, sema);
+    //initialize a new semaphore
+    sema->value = value;
+    sema->w_queue = create_queue();
+    sema_push(s_queue, sema);
 }
 
 void ta_sem_destroy(tasem_t* sema) {
+    //destroy semaphore
     if (len(sema->w_queue)){
-        sema_flag = 1;
+        sema_flag = 1;  //set flag to tell user thread left on sema_queue
     }
   	destroy_queue(sema->w_queue);
 }
 
 void ta_sem_signal(tasem_t* sema) {
+    //semaphore V(), decrement semaphore counter by 1.
     sema->value++;
     if (sema->value <= 0 && len(sema->w_queue)) {
 	tcb* tcb = pop(sema->w_queue);
@@ -106,33 +114,38 @@ void ta_sem_signal(tasem_t* sema) {
 }
 
 void ta_sem_wait(tasem_t* sema){
-  sema->value--;		
-  if (sema->value < 0){
-    tcb* to_wait = tcb_init();
-    push(sema->w_queue, to_wait);
-    
-    tcb* next_tcb = pop(t_queue);
-    ucontext_t* next_ctx = next_tcb->ctx;
-    free(next_tcb);
-    swapcontext(to_wait->ctx, next_ctx);
-  }	
+    //semaphore P(), increment semaphore counter by 1.
+    sema->value--;		
+    if (sema->value < 0){
+	tcb* to_wait = tcb_init();
+	push(sema->w_queue, to_wait);
+	
+	tcb* next_tcb = pop(t_queue);
+	ucontext_t* next_ctx = next_tcb->ctx;
+	free(next_tcb);
+	swapcontext(to_wait->ctx, next_ctx);
+    }	
 }
 
 void ta_lock_init(talock_t* lock){
-  lock->sem = (tasem_t*)malloc(sizeof(tasem_t));
+    //initialize lock using a binary semaphore
+    lock->sem = (tasem_t*)malloc(sizeof(tasem_t));
     ta_sem_init(lock->sem, 1);
 }
 
 void ta_lock_destroy(talock_t* lock){
+    //destroy lock
     ta_sem_destroy(lock->sem);
     free(lock->sem);
 }
 
 void ta_lock(talock_t* lock){
+    //lock the lock
     ta_sem_wait(lock->sem);
 }
 
 void ta_unlock(talock_t* lock){
+    //unlock the lock
     ta_sem_signal(lock->sem);
 }
 
